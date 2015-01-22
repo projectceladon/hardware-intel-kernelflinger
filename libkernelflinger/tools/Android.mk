323b880ecd3412aad151ebda6da8453cc725abe4
Andrew Boie <andrew.p.boie@intel.com>
2015-01-22 10:42:23 -0800

    png2c: fix leak, enforce -Werror
    Issue: GMINL-5394
    Change-Id: Id6810d72b62fbf6ada1842605ae8775b60e35cc9
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- libkernelflinger/tools/Android.mk
+++ libkernelflinger/tools/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := png2c.c
 LOCAL_STATIC_LIBRARIES := libpng libz
 LOCAL_C_INCLUDES += external/libpng
-LOCAL_CFLAGS += -O2 -g -Wall -pedantic
+LOCAL_CFLAGS += -O2 -g -Wall -Werror -pedantic
 LOCAL_MODULE := png2c
 
 include $(BUILD_HOST_EXECUTABLE)
