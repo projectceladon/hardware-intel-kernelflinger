9e836e0253a61161bc6db8e1c68dc56d5f23f942
Jeremy Compostella <jeremy.compostella@intel.com>
2015-10-28 13:23:17 +0100

    libfastboot: support erase on SATA drive
    kernelflinger uses the DATA SET MANAGEMENT command TRIM feature to
    erase the data.
    
    Tracked-On: OAM-7883
    Change-Id: Ia121c4079df0f43e3a0da2b040d20f2286873d40
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: 429533

--- libkernelflinger/sata.c
+++ libkernelflinger/sata.c
@@ -31,13 +31,200 @@
  */
 
 #include <lib.h>
+#include "protocol/AtaPassThru.h"
+#include "protocol/Atapi.h"
 #include "storage.h"
 
-static EFI_STATUS sata_erase_blocks(__attribute__((unused)) EFI_HANDLE handle,
-				    __attribute__((unused)) EFI_BLOCK_IO *bio,
-				    __attribute__((unused)) UINT64 start,
-				    __attribute__((unused)) UINT64 end)
+#define TRIM_SUPPORTED_BIT		0x01
+#define BIT5				0x20
+#define BIT6				0x40
+#define BIT7				0x80
+#define ATA_TIMEOUT_NS			30000000
+#define BLOCK_SIZE			0x200
+#define MAX_SECTOR_PER_RANGE		0xFFFF
+#define ATA_CMD_DSM_TRIM_FEATURE	0x1
+#define PORT_MULTIPLIER_POS		0x4
+
+typedef struct lba_range_entry {
+	UINT16 lba[3];
+	UINT16 len;
+} __attribute__((packed)) lba_range_entry_t;
+
+static SATA_DEVICE_PATH *get_sata_device_path(EFI_DEVICE_PATH *p)
 {
+	for (; !IsDevicePathEndType(p); p = NextDevicePathNode(p))
+		if (DevicePathType(p) == MESSAGING_DEVICE_PATH
+		    && DevicePathSubType(p) == MSG_SATA_DP)
+			return (SATA_DEVICE_PATH *)p;
+
+	return NULL;
+}
+
+static EFI_STATUS sata_identify_data(EFI_ATA_PASS_THRU_PROTOCOL *ata,
+				     SATA_DEVICE_PATH *sata_dp,
+				     ATA_IDENTIFY_DATA *identify_data)
+{
+	EFI_STATUS ret;
+	EFI_ATA_STATUS_BLOCK asb;
+	EFI_ATA_COMMAND_BLOCK acb = {
+		.AtaCommand = ATA_CMD_IDENTIFY_DRIVE,
+		.AtaDeviceHead = (UINT8) (BIT7 | BIT6 | BIT5 |
+					  (sata_dp->PortMultiplierPortNumber << PORT_MULTIPLIER_POS))
+	};
+	EFI_ATA_PASS_THRU_COMMAND_PACKET ata_packet = {
+		.Asb = &asb,
+		.Acb = &acb,
+		.Timeout = ATA_TIMEOUT_NS,
+		.Protocol = EFI_ATA_PASS_THRU_PROTOCOL_PIO_DATA_IN,
+		.Length = EFI_ATA_PASS_THRU_LENGTH_BYTES | EFI_ATA_PASS_THRU_LENGTH_SECTOR_COUNT,
+		.InDataBuffer = identify_data,
+		.InTransferLength = sizeof(*identify_data)
+	};
+
+	ret = uefi_call_wrapper(ata->PassThru, 5, ata,
+				sata_dp->HBAPortNumber,
+				sata_dp->PortMultiplierPortNumber,
+				&ata_packet, NULL);
+	if (EFI_ERROR(ret))
+		efi_perror(ret, L"Failed to get ATA_IDENTIFY_DATA");
+
+	return ret;
+}
+
+static BOOLEAN is_dsm_trim_supported(EFI_ATA_PASS_THRU_PROTOCOL *ata,
+				     SATA_DEVICE_PATH *sata_dp,
+				     UINT16 *max_dsm_block_nb)
+{
+	ATA_IDENTIFY_DATA identify_data;
+	EFI_STATUS ret;
+
+	ret = sata_identify_data(ata, sata_dp, &identify_data);
+	if (EFI_ERROR(ret))
+		return FALSE;
+
+	if (!(identify_data.is_data_set_cmd_supported & TRIM_SUPPORTED_BIT)
+	    || identify_data.max_no_of_512byte_blocks_per_data_set_cmd == 0) {
+		debug(L"This SATA device does support DATA SET MANAGEMENT command");
+		return FALSE;
+	}
+
+	*max_dsm_block_nb = identify_data.max_no_of_512byte_blocks_per_data_set_cmd;
+	return TRUE;
+}
+
+/* http://www.t13.org/documents/uploadeddocuments/docs2009/d2015r2-ataatapi_command_set_-_2_acs-2.pdf
+ * See. 7.10 DATA SET MANAG EMENT - 06h, DMA
+ * See. 4.18.3.2 LBA Range Entry
+ */
+static EFI_STATUS ata_dsm_trim(EFI_ATA_PASS_THRU_PROTOCOL *ata,
+			       SATA_DEVICE_PATH *sata_dp, UINT64 start, UINT64 end,
+			       UINT16 max_dsm_block_nb)
+{
+	EFI_STATUS ret = EFI_INVALID_PARAMETER;
+	EFI_ATA_STATUS_BLOCK asb;
+	EFI_ATA_COMMAND_BLOCK acb = {
+		.AtaCommand = ATA_CMD_DSM,
+		.AtaFeatures = ATA_CMD_DSM_TRIM_FEATURE,
+		.AtaDeviceHead = (UINT8) (BIT7 | BIT6 | BIT5 |
+					  (sata_dp->PortMultiplierPortNumber << PORT_MULTIPLIER_POS))
+	};
+	EFI_ATA_PASS_THRU_COMMAND_PACKET ata_packet = {
+		.Asb = &asb,
+		.Acb = &acb,
+		.Timeout = ATA_TIMEOUT_NS,
+		.Protocol = EFI_ATA_PASS_THRU_PROTOCOL_PIO_DATA_OUT,
+		.Length = EFI_ATA_PASS_THRU_LENGTH_BYTES | EFI_ATA_PASS_THRU_LENGTH_SECTOR_COUNT
+	};
+	lba_range_entry_t *range, *buf;
+	UINT64 left;
+	UINTN nr_sectors, nr_ranges, nr_blocks, i, count;
+
+	nr_sectors = end - start + 1;
+	nr_ranges = nr_sectors / MAX_SECTOR_PER_RANGE;
+	if (nr_sectors % MAX_SECTOR_PER_RANGE)
+		nr_ranges++;
+	nr_blocks = (nr_ranges * sizeof(*range)) / BLOCK_SIZE;
+	if ((nr_ranges * sizeof(UINT64)) % BLOCK_SIZE)
+		nr_blocks++;
+
+	ret = alloc_aligned((VOID **)&buf, (VOID **)&range,
+			    nr_blocks * BLOCK_SIZE, ata->Mode->IoAlign);
+	if (EFI_ERROR(ret)) {
+		error(L"Failed to allocate DSM LBA Range buffer");
+		return ret;
+	}
+
+	for (i = 0; start <= end; start += MAX_SECTOR_PER_RANGE, i++) {
+		*((UINT64 *)&range[i]) = start;
+		left = end - start + 1;
+		range[i].len = left < MAX_SECTOR_PER_RANGE ? left : MAX_SECTOR_PER_RANGE;
+	}
+
+	for (i = 0; i < nr_blocks; i += max_dsm_block_nb) {
+		ata_packet.OutDataBuffer = ((UINT8 *)range) + i * BLOCK_SIZE;
+
+		count = min(nr_blocks - i, max_dsm_block_nb);
+		ata_packet.OutTransferLength = count * BLOCK_SIZE;
+		acb.AtaSectorCount = count;
+
+		memset(&asb, 0, sizeof(asb));
+		ret = uefi_call_wrapper(ata->PassThru, 5, ata,
+					sata_dp->HBAPortNumber,
+					sata_dp->PortMultiplierPortNumber,
+					&ata_packet, NULL);
+		if (EFI_ERROR(ret)) {
+			efi_perror(ret, L"DATA SET MANAGEMENT command failed");
+			goto out;
+		}
+	}
+
+out:
+	FreePool(buf);
+	return ret;
+}
+
+static EFI_STATUS sata_erase_blocks(EFI_HANDLE handle,
+				    __attribute__((unused)) EFI_BLOCK_IO *bio,
+				    UINT64 start, UINT64 end)
+{
+	EFI_STATUS ret;
+	EFI_GUID AtaPassThruProtocolGuid = EFI_ATA_PASS_THRU_PROTOCOL_GUID;
+	EFI_DEVICE_PATH *dp;
+	EFI_HANDLE ata_handle;
+	SATA_DEVICE_PATH *sata_dp;
+	EFI_ATA_PASS_THRU_PROTOCOL *ata;
+	UINT16 max_dsm_block_nb;
+
+	dp = DevicePathFromHandle(handle);
+	if (!dp) {
+		error(L"Failed to get device path from handle");
+		return EFI_INVALID_PARAMETER;
+	}
+
+	sata_dp = (SATA_DEVICE_PATH *)dp;
+	ret = uefi_call_wrapper(BS->LocateDevicePath, 3, &AtaPassThruProtocolGuid,
+				(EFI_DEVICE_PATH **)&sata_dp, &ata_handle);
+	if (EFI_ERROR(ret)) {
+		efi_perror(ret, L"Failed to locate ATA root device");
+		return ret;
+	}
+
+	sata_dp = get_sata_device_path(dp);
+	if (!sata_dp) {
+		error(L"Failed to get ATA device path");
+		return EFI_NOT_FOUND;
+	}
+
+	ret = uefi_call_wrapper(BS->HandleProtocol, 3, ata_handle,
+				&AtaPassThruProtocolGuid, (void *)&ata);
+	if (EFI_ERROR(ret)) {
+		efi_perror(ret, L"failed to get ATA protocol");
+		return ret;
+	}
+
+	if (is_dsm_trim_supported(ata, sata_dp, &max_dsm_block_nb))
+		return ata_dsm_trim(ata, sata_dp, start, end, max_dsm_block_nb);
+
 	return EFI_UNSUPPORTED;
 }
 
@@ -49,13 +236,7 @@ static EFI_STATUS sata_check_logical_unit(__attribute__((unused)) EFI_DEVICE_PAT
 
 static BOOLEAN is_sata(EFI_DEVICE_PATH *p)
 {
-	while (!IsDevicePathEndType(p)) {
-		if (DevicePathType(p) == MESSAGING_DEVICE_PATH
-		    && DevicePathSubType(p) == MSG_SATA_DP)
-			return TRUE;
-		p = NextDevicePathNode(p);
-	}
-	return FALSE;
+	return get_sata_device_path(p) != NULL;
 }
 
 struct storage STORAGE(STORAGE_SATA) = {
