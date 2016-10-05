1475be20fe3ea32064dc6cd3a75f5456d0132e6c
Jeremy Compostella <jeremy.compostella@intel.com>
2016-10-05 15:13:18 +0200

    04.11
    Change-Id: I3923e9b1ae39ad4e549a5b0e8eed7d4675fbee95
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-38488
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://android.intel.com:443/543677

--- include/libkernelflinger/version.h
+++ include/libkernelflinger/version.h
@@ -44,7 +44,7 @@
 #define BUILD_VARIANT           "-eng"
 #endif
 
-#define KERNELFLINGER_VERSION_8	"kernelflinger-04.10" BUILD_VARIANT
+#define KERNELFLINGER_VERSION_8	"kernelflinger-04.11" BUILD_VARIANT
 #define KERNELFLINGER_VERSION   WIDE_STR(KERNELFLINGER_VERSION_8)
 
 #endif
