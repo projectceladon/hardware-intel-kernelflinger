f2171fe98921faf27a60732079c62d8969aeff47
Jeremy Compostella <jeremy.compostella@intel.com>
2016-11-21 14:29:10 +0100

    04.19
    Change-Id: I06b0db2e956e0a7a622baa06ec9990f0a59c6494
    Tracked-On: OAM-40354
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: 553397

--- include/libkernelflinger/version.h
+++ include/libkernelflinger/version.h
@@ -44,7 +44,7 @@
 #define BUILD_VARIANT           "-eng"
 #endif
 
-#define KERNELFLINGER_VERSION_8	"kernelflinger-04.18" BUILD_VARIANT
+#define KERNELFLINGER_VERSION_8	"kernelflinger-04.19" BUILD_VARIANT
 #define KERNELFLINGER_VERSION   WIDE_STR(KERNELFLINGER_VERSION_8)
 
 #endif
