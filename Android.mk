d97716ce48c0e88440e4c26d0750a6d2d21f7670
gli41 <genshen.li@intel.com>
2017-06-22 01:10:42 +0800

    Fix fb4abl build error when AVB is enabled
    Modify fb4abl related makefile to make it consistent
    with kf4abl when AVB is enabled.
    
    Change-Id: I5e9ad85460724153489e998e3597a0d9dd97e1fb
    Signed-off-by: gli41 <genshen.li@intel.com>
    Tracked-On: OAM-46820
    Reviewed-on: 589659

--- Android.mk
+++ Android.mk
@@ -209,16 +209,16 @@ LOCAL_SRC_FILES := \
 	kf4abl.c
 
 ifeq ($(BOARD_AVB_ENABLE),true)
-LOCAL_SRC_FILES	+= \
+LOCAL_SRC_FILES += \
 	avb_init.c
 endif
 
-ifeq ($(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_VERITY), true)
-kf4abl_intermediates := $(call intermediates-dir-for,ABL,kf4abl)
+ifeq ($(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_VERITY),true)
+keys4abl_intermediates := $(call intermediates-dir-for,ABL,keys)
 
-ABL_VERITY_CERT := $(kf4abl_intermediates)/verity.cer
-ABL_PADDED_VERITY_CERT := $(kf4abl_intermediates)/verity.padded.cer
-ABL_OEMCERT_OBJ := $(kf4abl_intermediates)/oemcert.o
+ABL_VERITY_CERT := $(keys4abl_intermediates)/verity.cer
+ABL_PADDED_VERITY_CERT := $(keys4abl_intermediates)/verity.padded.cer
+ABL_OEMCERT_OBJ := $(keys4abl_intermediates)/oemcert.o
 
 $(ABL_VERITY_CERT): $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).x509.pem $(OPENSSL)
 	$(transform-pem-cert-to-der-cert)
@@ -244,12 +244,12 @@ $(ABL_OEMCERT_OBJ): $(ABL_PADDED_VERITY_CERT)
 LOCAL_GENERATED_SOURCES := $(ABL_OEMCERT_OBJ)
 endif
 
-ifeq ($(BOARD_AVB_ENABLE), true)
-kf4abl_intermediates := $(call intermediates-dir-for,ABL,kf4abl)
+ifeq ($(BOARD_AVB_ENABLE),true)
+keys4abl_intermediates := $(call intermediates-dir-for,ABL,keys4abl)
 
-ABL_AVB_PK := $(kf4abl_intermediates)/avb_pk.bin
-ABL_PADDED_AVB_PK := $(kf4abl_intermediates)/avb_pk.padded.bin
-ABL_AVB_PK_OBJ := $(kf4abl_intermediates)/avb_pk.o
+ABL_AVB_PK := $(keys4abl_intermediates)/avb_pk.bin
+ABL_PADDED_AVB_PK := $(keys4abl_intermediates)/avb_pk.padded.bin
+ABL_AVB_PK_OBJ := $(keys4abl_intermediates)/avb_pk.o
 ifndef BOARD_AVB_KEY_PATH
 BOOTLOADER_AVB_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 else
@@ -275,7 +275,7 @@ $(ABL_AVB_PK_OBJ): $(ABL_PADDED_AVB_PK)
 	$(EFI_OBJCOPY) --redefine-sym $(avb_sym_binary)_start=_binary_avb_pk_start \
                        --redefine-sym $(avb_sym_binary)_end=_binary_avb_pk_end \
                        --redefine-sym $(avb_sym_binary)_size=_binary_avb_pk_size \
-                       --rename-section .data=.avbkeys $@ $@
+                       --rename-section .data=.oemkeys $@ $@
 
 LOCAL_GENERATED_SOURCES += $(ABL_AVB_PK_OBJ)
 LOCAL_C_INCLUDES := \
@@ -307,29 +307,25 @@ LOCAL_STATIC_LIBRARIES += \
 ifneq ($(TARGET_BUILD_VARIANT),user)
     LOCAL_STATIC_LIBRARIES += libadb-$(TARGET_BUILD_VARIANT)
 endif
+ifeq ($(BOARD_AVB_ENABLE),true)
+    LOCAL_STATIC_LIBRARIES += libavb_kernelflinger-$(TARGET_BUILD_VARIANT)
+endif
 LOCAL_SRC_FILES := \
 	kf4abl.c
-fb4abl_intermediates := $(call intermediates-dir-for,ABL,fb4abl)
 
-FB_ABL_VERITY_CERT := $(fb4abl_intermediates)/verity.cer
-FB_ABL_PADDED_VERITY_CERT := $(fb4abl_intermediates)/verity.padded.cer
-FB_ABL_OEMCERT_OBJ := $(fb4abl_intermediates)/oemcert.o
+ifeq ($(BOARD_AVB_ENABLE),true)
+LOCAL_SRC_FILES += \
+	avb_init.c
+endif
 
-$(FB_ABL_VERITY_CERT): $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).x509.pem $(OPENSSL)
-	$(transform-pem-cert-to-der-cert)
+ifeq ($(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_VERITY),true)
+LOCAL_GENERATED_SOURCES := $(ABL_OEMCERT_OBJ)
+endif
 
-$(FB_ABL_PADDED_VERITY_CERT): $(FB_ABL_VERITY_CERT)
-	$(call pad-binary, 4096)
-
-fb_abl_sym_binary := $(shell echo _binary_$(FB_ABL_PADDED_VERITY_CERT) | sed "s/[\/\.-]/_/g")
-$(FB_ABL_OEMCERT_OBJ): $(FB_ABL_PADDED_VERITY_CERT)
-	mkdir -p $(@D) && \
-	$(EFI_OBJCOPY) --input binary --output $(ELF_OUTPUT) --binary-architecture i386 $< $@ && \
-	$(EFI_OBJCOPY) --redefine-sym $(fb_abl_sym_binary)_start=_binary_oemcert_start \
-                       --redefine-sym $(fb_abl_sym_binary)_end=_binary_oemcert_end \
-                       --redefine-sym $(fb_abl_sym_binary)_size=_binary_oemcert_size \
-                       --rename-section .data=.oemkeys $@ $@
-
-LOCAL_GENERATED_SOURCES := $(FB_ABL_OEMCERT_OBJ)
+ifeq ($(BOARD_AVB_ENABLE),true)
+LOCAL_GENERATED_SOURCES += $(ABL_AVB_PK_OBJ)
+LOCAL_C_INCLUDES := \
+	$(addprefix $(LOCAL_PATH)/,avb)
+endif
 include $(BUILD_ABL_EXECUTABLE)
 
