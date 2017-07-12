88db29ca8958190ec343444ab7f6cf1ca337683a
adattatr <anisha.dattatraya.kulkarni@intel.com>
2017-07-11 18:36:06 -0700

    REVERTME: Disable decode_printable_string on non-USER builds
    M_d2i functions have been removed from boringssl.
    Temporarily disable calling this function.
    
    Change-Id: I25f7ec6c9c848b373222c11d814a7ac998f401d5
    Tracked-On: OAM-49240
    Reviewed-on: 597995

--- libkernelflinger/aosp_sig.c
+++ libkernelflinger/aosp_sig.c
@@ -73,10 +73,11 @@ static int decode_auth_attributes(const unsigned char **datap, long *sizep,
 	if (consume_sequence(datap, &seq_size) < 0)
 		return -1;
 
+#ifdef USER
 	if (decode_printable_string(datap, &seq_size, aa->target,
 				    sizeof(aa->target)))
 		return -1;
-
+#endif
 	if (decode_integer(datap, &seq_size, 0, &aa->length,
 			   NULL, NULL))
 		return -1;
