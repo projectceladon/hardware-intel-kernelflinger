b893ff6b55a86133e4f8237e468e86866b7b072f
Ming Tan <ming.tan@intel.com>
2018-03-19 13:11:14 +0800

    Let the kernelflinger assume the BIOS already enable secure boot.
    It is useful when the BIOS does not support secure boot, but still
    want kernelflinger maybe set the verify boot state to green.
    
    JIRA: AIA-418
    Test: Verify boot state set to green.
    
    Change-Id: Ic0425e670e73a9a9ac5b6e53f5a4d2d767af74a3
    Signed-off-by: Ming Tan <ming.tan@intel.com>

--- libkernelflinger/security_efi.c
+++ libkernelflinger/security_efi.c
@@ -51,10 +51,10 @@ EFI_STATUS set_platform_secure_boot(__attribute__((unused)) IN UINT8 secure)
    treated as read- only. */
 BOOLEAN is_platform_secure_boot_enabled(VOID)
 {
-        EFI_GUID global_guid = EFI_GLOBAL_VARIABLE;
+        return TRUE;
+/*      EFI_GUID global_guid = EFI_GLOBAL_VARIABLE;
         EFI_STATUS ret;
         UINT8 value;
-
         ret = get_efi_variable_byte(&global_guid, SETUP_MODE_VAR, &value);
         if (EFI_ERROR(ret))
                 return FALSE;
@@ -66,7 +66,7 @@ BOOLEAN is_platform_secure_boot_enabled(VOID)
         if (EFI_ERROR(ret))
                 return FALSE;
 
-        return value == 1;
+        return value == 1;*/
 }
 
 BOOLEAN is_eom_and_secureboot_enabled(VOID)
