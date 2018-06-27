ef59ef35370a28dff3fde9b10b3853187fbedc47
zhouji3x <jianfengx.zhou@intel.com>
2018-06-27 13:03:05 +0800

    [osloader] load trusty to high memory
    Page table is neccessary for restore linux memory map from dumped raw memory data.
    To find out page table from raw memory data, assume .text/.data/.rodata section of
    kernel, virtual memory ffffffff8100000-ffffffff83ffffff is mapped to physical
    memory 100000-3ffffff, so we can read page table from raw memory data via symbol
    tables included in the vmlinux.
    
    Currently, Osloader allocate memory using EFI service for trusty, which allocate
    memory from low memory area, low memory areas is key to store kernel page tables.
    
    This patch:
    1) alloc high free memory for trusty, so low memory area are usable for kernel
    2) reserve memory for trusty via EFI service.
    
    Change-Id: If06518628afe2a187fa828e11ed302f93d2c4858
    Tracked-On: OAM-66078
    Signed-off-by: zhouji3x <jianfengx.zhou@intel.com>
    Reviewed-on: 635790

--- libkernelflinger/trusty_sbl.c
+++ libkernelflinger/trusty_sbl.c
@@ -45,9 +45,11 @@
 #include "gpt.h"
 #include "efilinux.h"
 #include "libelfloader.h"
+#include <uefi_utils.h>
 
 #define TRUSTY_MEM_SIZE        0x1000000
-#define TRUSTY_MEM_ALIGNED_16K 0x4000
+#define TRUSTY_MEM_ALIGNED     (2*1024*1024)
+#define TRUSTY_MEM_MIN_ADDRESS 0x04000000
 #define TRUSTY_MEM_MAX_ADDRESS 0xFFFFFFFF
 
 typedef struct trusty_boot_param {
@@ -131,9 +133,74 @@ EFI_STATUS set_trusty_param(__attribute__((unused)) IN VOID *param_data)
 	return EFI_UNSUPPORTED;
 }
 
+static EFI_STATUS search_usable_memory(OUT EFI_PHYSICAL_ADDRESS *lp_mem,
+	IN UINT32 alloc_size, IN UINT32 align_size,
+	IN EFI_PHYSICAL_ADDRESS min_addr,
+	IN EFI_PHYSICAL_ADDRESS max_addr)
+{
+	EFI_MEMORY_DESCRIPTOR entries[64];
+	EFI_MEMORY_DESCRIPTOR *cur;
+	EFI_PHYSICAL_ADDRESS  start, end;
+	EFI_STATUS ret;
+	UINTN nr_entries;
+	UINTN entry_sz;
+	UINTN key;
+	UINTN i;
+	UINT32 descr_ver;
+
+	if (lp_mem == NULL)
+		return EFI_NOT_FOUND;
+
+	nr_entries = sizeof(entries);
+	ret = uefi_call_wrapper(BS->GetMemoryMap, 5, &nr_entries,
+				(EFI_MEMORY_DESCRIPTOR *)entries,
+				&key, &entry_sz, &descr_ver);
+	if (EFI_ERROR(ret)) {
+		efi_perror(ret, L"Failed to get the current memory map");
+		return ret;
+	}
+	nr_entries /= entry_sz;
+	sort_memory_map(entries, nr_entries, entry_sz);
+
+	*lp_mem = 0;
+	for (i = 0; i < nr_entries; i++) {
+		cur = (EFI_MEMORY_DESCRIPTOR *)(entries + i);
+		if (cur->Type != EfiConventionalMemory)
+			continue;
+
+		end = cur->PhysicalStart +
+			cur->NumberOfPages * EFI_PAGE_SIZE;
+		start = ALIGN(cur->PhysicalStart, align_size);
+
+		if (min_addr != 0 && max_addr > min_addr)
+		{
+			if (start + alloc_size + align_size > max_addr)
+				continue;
+			if (end < min_addr + alloc_size + align_size)
+				continue;
+
+			if (start < min_addr)
+				start = min_addr;
+
+			if (end > max_addr)
+				end = max_addr;
+		}
+
+		if (end - start < alloc_size + align_size)
+			continue;
+
+		*lp_mem = end - alloc_size;
+		*lp_mem = ALIGN_DOWN(*lp_mem, align_size);
+		break;
+	}
+
+	return *lp_mem ? EFI_SUCCESS : EFI_OUT_OF_RESOURCES;
+}
+
+
 EFI_STATUS start_trusty(VOID *tosimage)
 {
-        EFI_STATUS ret;
+	EFI_STATUS ret;
 	const struct boot_img_hdr *header;
 	UINTN load_base;
 	trusty_startup_params_t trusty_startup_params;
@@ -146,15 +213,21 @@ EFI_STATUS start_trusty(VOID *tosimage)
 	header = (const struct boot_img_hdr *)tosimage;
 	load_base = (UINTN)(tosimage + header->page_size);
 
-	Memory = (EFI_PHYSICAL_ADDRESS)(TRUSTY_MEM_MAX_ADDRESS - TRUSTY_MEM_SIZE - TRUSTY_MEM_ALIGNED_16K + 1);
-	ret = uefi_call_wrapper(BS->AllocatePages, 4, AllocateMaxAddress,
-				EfiRuntimeServicesData,  EFI_SIZE_TO_PAGES(TRUSTY_MEM_SIZE + TRUSTY_MEM_ALIGNED_16K), &Memory);
+	ret = search_usable_memory(&Memory, TRUSTY_MEM_SIZE, TRUSTY_MEM_ALIGNED,
+				TRUSTY_MEM_MIN_ADDRESS, TRUSTY_MEM_MAX_ADDRESS);
 	if (EFI_ERROR(ret)) {
 		efi_perror(ret, L"Failed to allocate trusty pages");
 		goto fail;
 	}
 
-	trusty_boot_params.trusty_mem_base = ((UINT64)Memory + TRUSTY_MEM_ALIGNED_16K - 1) & ~(TRUSTY_MEM_ALIGNED_16K - 1);
+	ret = uefi_call_wrapper(BS->AllocatePages, 4, AllocateAddress,
+				EfiRuntimeServicesData,  EFI_SIZE_TO_PAGES(TRUSTY_MEM_SIZE), &Memory);
+	if (EFI_ERROR(ret)) {
+		efi_perror(ret, L"Failed to allocate trusty pages");
+		goto fail;
+	}
+
+	trusty_boot_params.trusty_mem_base = Memory;
 	trusty_boot_params.trusty_mem_size = TRUSTY_MEM_SIZE;
 
 	ret = init_trusty_startup_params(&trusty_startup_params, load_base, header->kernel_size, &trusty_boot_params);
@@ -175,7 +248,7 @@ EFI_STATUS start_trusty(VOID *tosimage)
 	return ret;
 
 fail:
-	uefi_call_wrapper(BS->FreePages, 2, Memory, EFI_SIZE_TO_PAGES(TRUSTY_MEM_SIZE + TRUSTY_MEM_ALIGNED_16K));
+	uefi_call_wrapper(BS->FreePages, 2, Memory, EFI_SIZE_TO_PAGES(TRUSTY_MEM_SIZE));
 
 	return ret;
 }
