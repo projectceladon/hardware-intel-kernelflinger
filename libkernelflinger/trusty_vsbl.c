bc91af4625e3864250eca3894a5bd7ace4a9a6c7
kwen <kui.wen@intel.com>
2018-08-21 07:44:13 +0800

    RPMB: RPMB key passing
    Parse the RPMB key from vsbl and pass it to trusty
    
    Change-Id: I5f85b3055b00e7b82f9ad78caabb29274936090c
    Tracked-On: OAM-62325
    Signed-off-by: kwen <kui.wen@intel.com>
    Reviewed-on: 642346

--- libkernelflinger/trusty_vsbl.c
+++ libkernelflinger/trusty_vsbl.c
@@ -46,6 +46,7 @@
 #include "gpt.h"
 #include "efilinux.h"
 #include "libelfloader.h"
+#include "rpmb_storage.h"
 
 #define TRUSTY_MEM_SIZE			0x1000000
 #define TRUSTY_MEM_ALIGNED_16K		0x4000
@@ -107,6 +108,9 @@ static EFI_STATUS init_trusty_startup_params(trusty_startup_params_t *param, UIN
 	UINTN size, trusty_boot_param_t *boot_param)
 {
 	UINT64 entry_addr;
+	EFI_STATUS ret = EFI_SUCCESS;
+	UINT8 *out_key = NULL;
+	UINT8 number_derived_key = 0;
 
 	if (!param || !boot_param)
 		return EFI_INVALID_PARAMETER;
@@ -124,6 +128,16 @@ static EFI_STATUS init_trusty_startup_params(trusty_startup_params_t *param, UIN
 	param->version = TRUSTY_BOOT_PARAM_VERSION;
 	param->runtime_size = TRUSTY_MEM_SIZE;
 	memset(param->rpmb_key, 0x0, sizeof(param->rpmb_key));
+
+	ret = get_rpmb_derived_key(&out_key, &number_derived_key);
+	if (EFI_ERROR(ret)) {
+		efi_perror(ret, L"get_rpmb_derived_key failed");
+		return ret;
+	}
+
+	/* Currently valid size of RPMB is 32byte and pass one rpmb key to trusty */
+	if ((number_derived_key > 0) && out_key)
+		memcpy(param->rpmb_key, out_key, RPMB_KEY_SIZE);
 
 	return EFI_SUCCESS;
 }
@@ -180,6 +194,7 @@ EFI_STATUS start_trusty(VOID *tosimage)
 	}
 
 	ret = launch_trusty_os(&trusty_startup_params);
+	memset(trusty_startup_params.rpmb_key, 0, sizeof(trusty_startup_params.rpmb_key));
 	if (EFI_ERROR(ret)) {
 		efi_perror(ret, L"Failed to launch trusty os");
 		return ret;
