8343f6b8defc555d9f63444213a6efe3e177cfc1
Huang Yang <yang.huang@intel.com>
2019-02-19 10:17:32 +0800

    Set default value of end_user to TRUE
    end_user is set to TRUE if fails to read EOM and SB.
    This is for ACRN AaaG to be regarded as an end user image
    to retrieve AttKB.
    
    Change-Id: Ifec72e251fd55629dfa8114a2b6a60daf3de02fc
    Signed-off-by: Huang Yang <yang.huang@intel.com>
    Tracked-On: OAM-76385
    Reviewed-on: 661742

--- libqltipc/ql-tipc/keymaster.c
+++ libqltipc/ql-tipc/keymaster.c
@@ -30,6 +30,7 @@
 #include "security.h"
 #include <life_cycle.h>
 #include <libqltipc/libtipc.h>
+#include "storage.h"
 
 #define LOCAL_LOG 0
 #define UNUSED(x) (void)(x)
@@ -322,7 +323,10 @@ int km_tipc_init(struct trusty_ipc_dev *dev)
     BOOLEAN enduser = false;
     EFI_STATUS ret = life_cycle_is_enduser(&enduser);
     if (EFI_ERROR(ret)) {
-        trusty_error("Failed to get eom var");
+        trusty_error("Failed to get eom var.\n");
+        /* For AaaG, set enduser to TRUE for AttKB retrieval */
+        if (is_boot_device_virtual())
+            enduser = true;
     }
 
     /* keybox not privisioned yet and is end user, then provision it */
